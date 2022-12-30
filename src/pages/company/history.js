a67b010b7724d8596cf799b0b33743287fb34c11
cvine <rafarakh@gmail.com>
2022-12-30 12:38:28 +0300

    feat: 🎸 page links
    Now it's possible to switch pages by clicking on the links

--- src/pages/company/history.js
+++ src/pages/company/history.js
@@ -1,7 +1,13 @@
+import { Link } from "gatsby";
 import React from "react";
 
 function History() {
-  return <div>History</div>;
+  return (
+    <div>
+      <h2>History</h2>
+      <Link to="/">home page</Link>
+    </div>
+  );
 }
 
 export default History;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
+import { Link } from "gatsby";
 import * as React from "react";
+import Layout from "../components/Layout";
 
 export default function Home() {
-	return <div>Hello world!</div>;
+  return (
+    <Layout>
+      <h1>Homepage</h1>
+      <div>
+        <Link to="/about">about</Link>
+      </div>
+      <div>
+        <Link to="/company/history">history</Link>
+      </div>
+    </Layout>
+  );
 }
