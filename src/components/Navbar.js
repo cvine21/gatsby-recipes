51e5541c6c10d4d770b713e2d15073bcce2579e0
cvine <rafarakh@gmail.com>
2023-01-01 21:47:35 +0300

    feat: 🎸 navbar
    navbar styles and logic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,51 @@
 import { Link } from "gatsby";
-import React from "react";
+import React, { useState } from "react";
+import { FiAlignJustify } from "react-icons/fi";
+
+import logo from "../assets/images/logo.svg";
 
 function Navbar() {
+  const [show, setShow] = useState(false);
+
+  const onShow = () => {
+    setShow((state) => !state);
+  };
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">home</Link>
-        </li>
-        <li>
-          <Link to="/about">about</Link>
-        </li>
-        <li>
-          <Link to="/contact">contact</Link>
-        </li>
-        <li>
-          <Link to="/tags">tags</Link>
-        </li>
-        <li>
-          <Link to="/recipes">about</Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+          <button className="nav-btn" onClick={onShow}>
+            <FiAlignJustify />
+          </button>
+        </div>
+        <div className={`nav-links ${show ? "show-links" : ""}`}>
+          <Link to="/" className="nav-link" activeClassName="active-link">
+            Home
+          </Link>
+          <Link
+            to="/recipes"
+            className="nav-link"
+            activeClassName="active-link"
+          >
+            Recipes
+          </Link>
+          <Link to="/tags" className="nav-link" activeClassName="active-link">
+            Tags
+          </Link>
+          <Link to="/about" className="nav-link" activeClassName="active-link">
+            About
+          </Link>
+          <div className="nav-link contact-link">
+            <Link to="/contact" className="btn">
+              Contact
+            </Link>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 }
--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Layout from "../components/Layout";
 
 function Tags() {
-  return <div>tags</div>;
+  return (
+    <Layout>
+      <div>tags</div>
+    </Layout>
+  );
 }
 
 export default Tags;
