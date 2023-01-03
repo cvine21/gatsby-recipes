a538216489b02109151e8b88858bef022f9af840
cvine <rafarakh@gmail.com>
2023-01-03 16:40:15 +0300

    feat: 🎸 about page layout
    about page article and image have been added

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,11 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`, `gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 };
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,35 @@
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import Layout from "../components/Layout";
 
 function About() {
   return (
     <Layout>
-      <h2>About</h2>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>I'm baby coloring book poke taxidermy</h2>
+            <p>
+              Taxidermy forage glossier letterpress heirloom before they sold
+              out you probably haven't heard of them banh mi biodiesel chia.
+            </p>
+            <p>
+              Taiyaki tumblr flexitarian jean shorts brunch, aesthetic salvia
+              retro.
+            </p>
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="Person pouring salt in bowl"
+            placeholder="blurred"
+            className="about-img"
+          />
+        </section>
+      </main>
     </Layout>
   );
 }
